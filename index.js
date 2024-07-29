53164271afe8740ce9c2367979461845c09d7e10
Akash632 <akashnagineni632@gmail.com>
2024-07-29 19:10:59 +0530

    latest commit

---
+++ index.js
@@ -0,0 +1,21 @@
+if(5>10){
+    console.log("Hello");
+}else if(5<10){
+    console.log("Hello world");
+}else{
+    console.log("hey");
+}
+
+// above 90 - A+
+// 80 and 90 - A
+// 70 and 80 - B+
+// 60 and 70 - B
+// FAIL
+
+// &&, >, <, >=, <=
+
+
+
+
+
+
